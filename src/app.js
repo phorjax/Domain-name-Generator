27dc6cb2410634c9b1c34b03df9cb6d31262b9bc
phorjax <joseph.12082@gmail.com>
2023-02-19 18:41:02 +0000

    hello there

--- src/app.js
+++ src/app.js
@@ -7,10 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  var Noun = ["history", "away", "art"];
-  var Adjective = ["agreeable", "adorable", "cloudy"];
-  var Pronoun = ["I", "she", "we"];
-  for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-  }
+  let DomainGen = () => {
+    var Noun = ["history", "away", "art"];
+    var Adjective = ["agreed", "adore", "had"];
+    var Pronoun = ["I", "she", "we"];
+
+    for (let i = 0; i < Pronoun.length; i++) {
+      document.querySelector("#DomainGen").innerHTML = Pronoun[i];
+
+      for (let j = 0; j < i.length; j++) {
+        // document.querySelector("#excuse").innerHTML = excuseGen();
+      }
+    }
+  };
 };
