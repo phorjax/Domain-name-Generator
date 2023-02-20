ccb0e6098a105bc041f2b062f717cd186e911e56
phorjax <joseph.12082@gmail.com>
2023-02-20 23:44:49 +0000

    doone

--- src/app.js
+++ src/app.js
@@ -7,19 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let DomainGenerator = () => {
-    var Noun = ["history", "art"];
-    var Adjective = ["adore", "had"];
-    var Pronoun = ["I", "she", "we"];
 
-    for (let i = 0; i < Pronoun.length; i++) {
-      for (let j = 0; j < Adjective.length; j++) {
-        for (let o = 0; o < Noun.length; o++) {
-          console.log(Pronoun[i] + Adjective[j] + Noun[o] + ".com");
-        }
+  var Noun = ["history", "art"];
+  var Adjective = ["adore", "had"];
+  var Pronoun = ["I", "she", "we"];
+  var dumblist = [];
+  for (let i = 0; i < Pronoun.length; i++) {
+    for (let j = 0; j < Adjective.length; j++) {
+      for (let o = 0; o < Noun.length; o++) {
+        dumblist.push(Pronoun[i] + Adjective[j] + Noun[o] + ".com");
       }
     }
-    return Pronoun + Adjective + Noun + ".com";
-  };
-  document.querySelector("#DomainGen").innerHTML = DomainGenerator();
+  }
+
+  let element = document.querySelector("#DomainGen");
+  for (let i = 0; i < dumblist.length; i++) {
+    element.innerHTML += "<li>" + dumblist[i] + "</li>";
+  }
 };
